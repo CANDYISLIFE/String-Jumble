c8f0ac609de1839af4d4b815e1e0e2d8917aa422
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-03-07 14:27:56 -0500

    Updated from Brython Server: 3/7/2016 2:27:56 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,6 +1,6 @@
 """
 stringjumble.py
-Author: <your name>
+Author: Billy
 Credit: <sources>
 
 Assignment:
@@ -25,3 +25,7 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+userString = input("Enter a word, sentence or paragraph.")
+
+stringLength = int( 

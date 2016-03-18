5e0deb965e07fbd32cee071f29d101ae3e2af36c
CANDYISLIFE <billybender@hanovernorwichschools.org>
2016-03-17 20:53:49 -0400

    Updated from Brython Server: 3/17/2016 8:53:48 PM

--- stringjumble.py
+++ stringjumble.py
@@ -27,7 +27,7 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 
 userInput = input("Please enter a string of text (the bigger the better): ")
-print("You entered "" + input + "". Now jumble it:")
+print("You entered " +'"' + userInput +'"'+ ". Now jumble it:")
 word= ""
 words = []
 wordsBackwards=[]
